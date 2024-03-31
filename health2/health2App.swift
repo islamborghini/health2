95e8b7156067e59b83e0297a785dad2b6684bc43
Islam Assanov <islamassanov66@gmail.com>
2024-03-31 09:08:14 -0700

    Initial Commit

---
+++ health2/health2App.swift
@@ -0,0 +1,17 @@
+//
+//  health2App.swift
+//  health2
+//
+//  Created by Islam Assanov on 31.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct health2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
